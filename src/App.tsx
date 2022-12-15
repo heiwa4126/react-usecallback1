2af06857970b81aca5703132277b78916a58b186
heiwa4126 <heiwa4126@gmail.com>
2022-12-15 17:36:40 +0900

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// from https://www.w3schools.com/react/react_usecallback.asp
+import "./App.css";
+import App1 from "./components/App1";
+
+const App = () => {
+  return (
+    <>
+      <App1 />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/App1.tsx
@@ -0,0 +1,29 @@
+// from https://www.w3schools.com/react/react_usecallback.asp
+import { useState } from "react";
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState<string[]>([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
+  return (
+    <>
+      <h2>My Todos (wrong)</h2>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
+
+export default App;
